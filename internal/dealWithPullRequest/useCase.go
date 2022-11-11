ba7614557e96a5db0cb5ea171c52c97d71dcd9ee
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-11-11 14:38:01 +0000

    Parse version and reject anything that looks unstable

--- internal/dealWithPullRequest/useCase.go
+++ internal/dealWithPullRequest/useCase.go
@@ -1,9 +1,15 @@
 package dealWithPullRequest
 
+import (
+	"regexp"
+	"strings"
+)
+
 type PullRequest struct {
 	Sender     string
 	Number     uint32
 	Repository string
+	Title      string
 }
 
 type Event struct {
@@ -43,13 +49,35 @@ func (d dependencies) Execute(presenter Presenter) {
 		return
 	}
 
+	isUnstable := regexp.MustCompile(`[A-Za-z]+`).MatchString
+	version := getVersionFromTitle(d.Event.PullRequest.Title)
+
+	body := "@dependabot merge"
+
+	if isUnstable(*version) {
+		body = "@dependabot close"
+	}
+
 	presenter.PostComment(
 		Comment{
 			Repository: d.repository(),
-			Body:       "@dependabot merge",
+			Body:       body,
 			Number:     d.prNumber(),
 		},
 	)
+}
+
+func getVersionFromTitle(prTitle string) *string {
+	var result string
+	if strings.Contains(prTitle, " to ") {
+		lastPart := strings.SplitAfter(prTitle, " from ")[1]
+		beforeSubDirectory := strings.SplitAfter(lastPart, " in ")[0]
+		beforeSubDirectory = strings.TrimSuffix(beforeSubDirectory, " in ")
+		version := strings.SplitAfter(beforeSubDirectory, " to ")[1]
+		result = strings.TrimPrefix(version, "v")
+		return &result
+	}
+	return nil
 }
 
 func (d dependencies) author() string {
--- internal/dealWithPullRequest/useCase_test.go
+++ internal/dealWithPullRequest/useCase_test.go
@@ -1,6 +1,8 @@
 package dealWithPullRequest
 
-import "testing"
+import (
+	"testing"
+)
 
 type SpyPresenter struct {
 	ExitCalled bool
@@ -54,17 +56,11 @@ func (uc dependencies) ExecuteWithSpy() SpyPresenter {
 	return presenter
 }
 
-func TestExitsIfNotPullRequestTargetEvent(t *testing.T) {
+func TestDealWithPullRequest(t *testing.T) {
 	t.Run("Can exit if not a pull request target event", func(t *testing.T) {
 		dealWithPullRequest := New(Event{Name: "not_expected"})
 		presenter := dealWithPullRequest.ExecuteWithSpy()
 		presenter.AssertExitCalled(t)
-	})
-
-	t.Run("Does not exit if pull request target event", func(t *testing.T) {
-		dealWithPullRequest := New(Event{Name: "pull_request_target", PullRequest: &PullRequest{Sender: "dependabot[bot]"}})
-		presenter := dealWithPullRequest.ExecuteWithSpy()
-		presenter.AssertExitNotCalled(t)
 	})
 
 	t.Run("Exits if not dependabot", func(t *testing.T) {
@@ -78,10 +74,12 @@ func TestExitsIfNotPullRequestTargetEvent(t *testing.T) {
 			Sender:     "dependabot[bot]",
 			Number:     1,
 			Repository: "madetech/wow",
+			Title:      "Bump python from 3.10.7 to 3.11.0",
 		}
 		dealWithPullRequest := New(Event{Name: "pull_request_target", PullRequest: &pr})
 		presenter := dealWithPullRequest.ExecuteWithSpy()
 		presenter.AssertCommented(t, "madetech/wow", 1, "@dependabot merge")
+		presenter.AssertExitNotCalled(t)
 	})
 
 	t.Run("Tells dependabot to merge 2", func(t *testing.T) {
@@ -89,9 +87,54 @@ func TestExitsIfNotPullRequestTargetEvent(t *testing.T) {
 			Sender:     "dependabot[bot]",
 			Number:     2,
 			Repository: "madetech/cool",
+			Title:      "Bump python from 3.10.7 to 3.11.0",
 		}
 		dealWithPullRequest := New(Event{Name: "pull_request_target", PullRequest: &pr})
 		presenter := dealWithPullRequest.ExecuteWithSpy()
 		presenter.AssertCommented(t, "madetech/cool", 2, "@dependabot merge")
+	})
+
+	t.Run("Tells dependabot to close", func(t *testing.T) {
+		pr := PullRequest{
+			Sender:     "dependabot[bot]",
+			Number:     1,
+			Repository: "madetech/wow",
+			Title:      "Bump python from 3.10.7 to 3.11.0rc2",
+		}
+		dealWithPullRequest := New(Event{Name: "pull_request_target", PullRequest: &pr})
+		presenter := dealWithPullRequest.ExecuteWithSpy()
+		presenter.AssertCommented(t, "madetech/wow", 1, "@dependabot close")
+	})
+}
+
+func assertVersionIsNil(t *testing.T, input string) {
+	t.Helper()
+	version := getVersionFromTitle(input)
+	if version != nil {
+		t.Errorf("Expected nil got %#v", version)
+	}
+}
+
+func assertExtractsVersion(t *testing.T, input string, expected string) {
+	t.Helper()
+	version := *getVersionFromTitle(input)
+	if version != expected {
+		t.Errorf("Expected %#v got %#v", expected, version)
+	}
+}
+
+func TestParseTitle(t *testing.T) {
+	t.Run("can parse", func(t *testing.T) {
+		assertVersionIsNil(t, "")
+		assertVersionIsNil(t, "Some title with no version")
+		assertExtractsVersion(t, "Bump dependency from 1.0.0 to 2.0.0", "2.0.0")
+		assertExtractsVersion(t, "Bump dependency from 1.0.0 to 3.0.0", "3.0.0")
+		assertExtractsVersion(t, "Bump to from 1.0.0 to 3.0.0", "3.0.0")
+		assertExtractsVersion(t, "Bump python from 3.10.7 to 3.11.0rc2", "3.11.0rc2")
+		assertExtractsVersion(t, "Build(deps-dev): Bump boto3 from 1.26.6 to 1.26.7 in /some/thing", "1.26.7")
+		assertExtractsVersion(t, "Build(deps): Bump boto3 from 1.26.4 to 1.26.5 in /a-thing", "1.26.5")
+		assertExtractsVersion(t, "Bump github.com/go-redis/redis/v9 from 9.0.0-beta.3 to 9.0.0-rc.1 in /one/two-three", "9.0.0-rc.1")
+		assertExtractsVersion(t, "Bump envoyproxy/envoy from v1.23.1 to v1.24.0 in /four/five-six", "1.24.0")
+		assertExtractsVersion(t, "Bump google.golang.org/grpc from 1.50.0 to 1.50.1 in /a/b/c/d/e/f/g", "1.50.1")
 	})
 }
